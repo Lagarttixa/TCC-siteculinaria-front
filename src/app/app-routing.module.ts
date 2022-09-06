479a512d146f8c1238e514cdef7295e4d18e770d
Lagarttixa <abner.cars@hotmail.com>
2022-09-06 20:41:41 -0300

    primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+{
+  path: 'login',
+  loadChildren: () => import('./login/route/login.module').then((m) => m.LoginModule)
+}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
+
